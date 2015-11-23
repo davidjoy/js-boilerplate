d7ea6c0e6c645b075f88b951b4213029ff172b67
David Joy <davjoy@gmail.com>
2015-11-23 18:09:46 -0500

    Getting an express server in there too.  Still wth hot-reloading.
    Checking in so I can go work on it at home.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,47 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: "./index.js",
-    devtool: 'source-map',
+    devtool: 'eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client?reload=true',
+        path.join(__dirname, 'client/main.js')
+    ],
     output: {
-        path: __dirname,
-        filename: "bundle.js"
+        path: path.join(__dirname, '/dist/'),
+        filename: '[name].js',
+        publicPath: '/'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'client/index.tpl.html',
+            inject: 'body',
+            filename: 'index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
     module: {
-        loaders: [
-            {test: /\.css$/, loader: "style!css"},
-            {test: /\.less$/, loader: "style!css!less"},
-            {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-        ]
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel?optional=es7.decorators'
+        }, {
+            test: /\.json?$/,
+            loader: 'json'
+        }, {
+            test: /\.css$/,
+            loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+        }, {
+            test: /\.less$/,
+            loader: "style!css!less?modules&localIdentName=[name]---[local]---[hash:base64:5]"
+        }]
     }
 };
